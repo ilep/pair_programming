58bb327133b9a963f883d809aaf4d8e7e06d6699
ilep <ivanlptr@gmail.com>
2014-10-28 13:22:14 +0100

    debut kruskal

--- KruskalAlgorithm.py
+++ KruskalAlgorithm.py
@@ -10,9 +10,10 @@ class KruskalAlgorithm:
     
     def __init__(self, graph):
         self.graph = graph
+        self.nodes_sets = self.__make_sets()
+        self.sorted_edges = self.__sorts_edges()
         
-    
-    def sort_edges(self):
+    def __sorts_edges(self):
         """
         Returns sorted list of edges. 
         """
@@ -21,3 +22,20 @@ class KruskalAlgorithm:
             raise ValueError('the graph has no edge.')
         else:
             return self.graph.edges.sort()
+    
+    def __make_sets(self):
+        nodes_sets = {}        
+        for node in self.graph.nodes:
+            nodes_sets[node.index] = set([node])
+        
+    
+    def kruskal(self):
+        """
+        Computes kruskal algorithm on graph. Returns minimal spanning tree A.
+        """
+        
+        A = graph() # Minimal spanning tree.       
+        for edge in self.sorted_edges:
+                        
+            print 'hey'
+            
